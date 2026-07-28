5f3223b1d259abdeb4e9fd1787e1639258a1c5eb
Valery Zinchenko <minicablestone@gmail.com>
2026-07-28 03:51:44 +0200

    chore: remove header logo

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -5,7 +5,6 @@ export function Header() {
     <header className="header">
       <div className="header__inner">
         <div className="header__brand">
-          <div className="header__logo">FM</div>
           <div className="header__brand-text">
             <span className="header__name">Valerý Zinčenko</span>
             <span className="header__tagline">systems engineering</span>
